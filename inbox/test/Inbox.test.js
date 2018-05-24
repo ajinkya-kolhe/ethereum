b35e5ead95f81492368110c6d81762670a625caa
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2018-05-24 19:44:53 +1000

    Assert default message and fix the test warning caused due to listeners issue.

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,17 +3,25 @@ const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
 const { interface, bytecode } = require('../compile')
+require('events').EventEmitter.defaultMaxListeners = 15;
 
+const DEFAULT_MESSAGE = 'Hello World'
 let accounts, inbox
-beforeEach(async() => {
+
+beforeEach(async () => {
   accounts = await web3.eth.getAccounts()
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hello World'] })
+    .deploy({ data: bytecode, arguments: [DEFAULT_MESSAGE] })
     .send({ from: accounts[0], gas: '1000000' })
 })
 
 describe('Inbox', () => {
 	it('should deploy a contract', () => {
     assert.ok(inbox.options.address)
+  })
+
+  it('should get a default message', async () => {
+    const message = await inbox.methods.message().call()
+    assert.equal(message, DEFAULT_MESSAGE)
   })
 })
